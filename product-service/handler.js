1690a0ae118e6a9abf788af9d79448f32ab65c2d
SzymonRegula <szymon.regula@gmail.com>
2024-02-28 20:07:43 +0100

    task 4.2

--- product-service/handler.js
+++ product-service/handler.js
@@ -2,27 +2,93 @@
 
 const products = require("./mockData");
 
-module.exports.getProductsList = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.getProductsList = async (event) => {
+  const productsParams = {
+    TableName: process.env.PRODUCTS_TABLE_NAME,
+  };
+  const stocksParams = {
+    TableName: process.env.STOCKS_TABLE_NAME,
+  };
+  try {
+    const productsDataPromise = dynamoDb.scan(productsParams).promise();
+    const stocksDataPromise = dynamoDb.scan(stocksParams).promise();
+
+    const [productsData, stocksData] = await Promise.all([
+      productsDataPromise,
+      stocksDataPromise,
+    ]);
+
+    const mergedData = productsData.Items.map((product) => {
+      const stock = stocksData.Items.find(
+        (item) => item.product_id === product.id
+      );
+      return {
+        ...product,
+        count: stock ? stock.count : 0,
+      };
+    });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(mergedData),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Cannot fetch products" }),
+    };
+  }
 };
 
 module.exports.getProductsById = async (event) => {
   const { productId } = event.pathParameters;
-  const product = products.find((p) => p.id === productId);
-  if (!product) {
+  const productParams = {
+    TableName: process.env.PRODUCTS_TABLE_NAME,
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": productId,
+    },
+  };
+  const stockParams = {
+    TableName: process.env.STOCKS_TABLE_NAME,
+    KeyConditionExpression: "product_id = :product_id",
+    ExpressionAttributeValues: {
+      ":product_id": productId,
+    },
+  };
+
+  try {
+    const productDataPromise = dynamoDb.query(productParams).promise();
+    const stockDataPromise = dynamoDb.query(stockParams).promise();
+
+    const [productData, stockData] = await Promise.all([
+      productDataPromise,
+      stockDataPromise,
+    ]);
+
+    const product = productData?.Items[0];
+    const stock = stockData?.Items[0];
+
+    const mergedProduct = {
+      ...product,
+      count: stock.count ?? 0,
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(mergedProduct),
+    };
+  } catch (error) {
+    console.error(error);
     return {
       statusCode: 404,
-      body: JSON.stringify({ message: "Product not found" }),
+      body: JSON.stringify({
+        message: `Cannot find product with id: ${productId}`,
+      }),
     };
   }
-  return {
-    statusCode: 200,
-    body: JSON.stringify(product),
-  };
 };
