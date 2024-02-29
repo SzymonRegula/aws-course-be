79e4b554175aae81a214728c56de57c48bce5893
SzymonRegula <szymon.regula@gmail.com>
2024-02-29 17:50:14 +0100

    task 5.4

--- import-service/handler.js
+++ import-service/handler.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const AWS = require("aws-sdk");
+const csv = require("csv-parser");
 const s3 = new AWS.S3({ region: "us-east-1" });
 
 module.exports.importProductsFile = async (event) => {
@@ -28,4 +29,37 @@ module.exports.importProductsFile = async (event) => {
       body: JSON.stringify({ message: "Error generating signed URL" }),
     };
   }
+};
+
+module.exports.importFileParser = async (event) => {
+  const promises = event.Records.map(
+    (record) =>
+      new Promise((resolve, reject) => {
+        try {
+          const s3Stream = s3
+            .getObject({
+              Bucket: record.s3.bucket.name,
+              Key: record.s3.object.key,
+            })
+            .createReadStream();
+
+          console.log(`Starting to process file ${record.s3.object.key}...`);
+
+          s3Stream
+            .pipe(csv())
+            .on("data", (data) => {
+              console.log(data);
+            })
+            .on("end", () => {
+              console.log(`File ${record.s3.object.key} has been processed.`);
+              resolve();
+            });
+        } catch (error) {
+          console.error("Error processing file:", error);
+          reject(error);
+        }
+      })
+  );
+
+  await Promise.all(promises);
 };
